3cf23f4bce65ce0e977efffe9eb38a55826b621b
Matheus Abreu <matheusabreu.dev@gmail.com>
2019-02-24 01:37:44 +0000

    refactor(Family): using props.children

--- src/components/family.jsx
+++ src/components/family.jsx
@@ -1,11 +1,3 @@
 import React from 'react'
 
-import Member from './member'
-
-export default props => (
-  <div>
-    <Member name='Matheus' lastName='Abreu' />
-    <Member name='Biel' lastName='Abreu' />
-    <Member name='Sosô' lastName='Abreu' />
-  </div>
-)
+export default props => <div>{props.children}</div>
--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Family from './components/family'
+import Member from './components/member'
 
-ReactDOM.render(<Family />, document.getElementById('app'))
+ReactDOM.render(
+  <Family>
+    <Member name='Matheus' lastName='Abreu' />
+    <Member name='Biel' lastName='Abreu' />
+    <Member name='Sosô' lastName='Abreu' />
+  </Family>,
+  document.getElementById('app')
+)
