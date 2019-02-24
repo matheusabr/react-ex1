a37c3d93acb898ba2b1b2a24348971477d3a62ea
Matheus Abreu <matheusabreu.dev@gmail.com>
2019-02-24 01:16:27 +0000

    feat: using multi components in a single file

---
+++ src/components/multComp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const FirstComp = props => <h4>First - Multi Component</h4>
+
+const SecondComp = props => <h4>Second - Multi Component</h4>
+
+export { FirstComp, SecondComp }
--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Comp1 from './components/comp1'
+import { FirstComp, SecondComp } from './components/multComp'
 
-ReactDOM.render(<Comp1 number={1} />, document.getElementById('app'))
+ReactDOM.render(
+  <div>
+    <Comp1 number={1} />
+    <FirstComp />
+    <SecondComp />
+  </div>,
+  document.getElementById('app')
+)
